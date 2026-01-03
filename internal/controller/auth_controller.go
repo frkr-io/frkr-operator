b552cd28ff772ac04453e5d03bcb0a2655c9a349
Jason DeMorrow <jason@jasondemorrow.com>
2026-01-02 22:27:42 -0800

    Remove unused imports from controllers

--- internal/controller/auth_controller.go
+++ internal/controller/auth_controller.go
@@ -2,9 +2,7 @@ package controller
 
 import (
 	"context"
-	"fmt"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
--- internal/controller/controllers.go
+++ internal/controller/controllers.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
 
